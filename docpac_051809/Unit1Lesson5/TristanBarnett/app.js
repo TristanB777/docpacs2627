7644a23a4cee70bb62c2b93b28c2b9f5b982c1fd
TristanB777 <28TBarnett@live.ytech.edu>
2026-09-17 14:54:17 -0400

    Idk why .gitignore broke

---
+++ docpac_051809/Unit1Lesson5/TristanBarnett/app.js
@@ -0,0 +1,42 @@
+const http = require('http');
+require('dotenv').config()
+const fs = require('fs');
+const PORT = Number(process.env.PORT);
+let url = require('url');
+const { basename } = require('path');
+baseUrl = "http://localhost:5000"
+
+const server = http.createServer((req,res) => {
+    if (req.url == '/') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end("hello world!\n");
+        
+    }
+    if (req.url == '/form') {
+            if (req.method === "POST") {
+                let body = ''
+            }
+            else if (req.method === "GET") {
+                fs.readFile('pages/form.html', 'utf8', (err, data) => {
+                    if (err) {
+                        console.error("error reading file",err);
+                        return;
+                    }
+                    res.write(data);
+                })
+            }
+            else {
+                res.writeHead(404, {  'Content-Type': 'text/plain'});
+                res.end("Page not found\n");
+            }
+        
+    }
+    if (req.method === 'GET') {
+        if
+    }
+
+});
+
+server.listen(PORT, 'localhost', () => {
+    console.log(`server running at http://localhost:${PORT}/`);
+})
